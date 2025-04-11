c8b781c6abc923627d5d5101368b6e069fd579dc
HoshMediaDev <hosh.media.dev@gmail.com>
2025-04-11 11:25:43 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,9 @@
-// custom.js
-
 function modifyImage() {
     const imageElement = document.querySelector('.n-avatar img');
+    
     if (imageElement) {
-        imageElement.style.filter = 'invert(0)';
-        console.log("Image modified with filter 'invert(0)'.");
-    } else {
-        console.log("No image found to modify.");
+        imageElement.style.filter = 'invert(1)';
     }
 }
+
+modifyImage();
